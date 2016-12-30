3e674d4a90b16b1044241d29556136931309b973
Flemming Madsen <fm@fmmbp.amplexfil.dk>
2016-12-30 17:37:48 +0100

    Signals for testing pololu driver & RC servo

--- xytest/xytest.ino
+++ xytest/xytest.ino
@@ -19,6 +19,7 @@ void setup(){
    pinMode(A3, OUTPUT);
    pinMode(A4, OUTPUT);
    pinMode(A5, OUTPUT);
+   Serial.begin(57600);
    Serial.print("A0 "); Serial.println(A0);
    Serial.print("A1 "); Serial.println(A1);
    Serial.print("A5 "); Serial.println(A5);
@@ -32,6 +33,7 @@ long cntMilli = 0;
 long cntCenti = 0;
 long cntSec = 0;
 long loopCnt = 0;
+long milli5 = 0;
 void loop(){
    long micro = micros();
 
@@ -41,23 +43,25 @@ void loop(){
    if (micro - microMilli < 200)
       return;
    microMilli += 200;
+   milli5++;
+   digitalWrite(19, (milli5&1) ? HIGH : LOW);  // Basic accuracy
    digitalWrite(3+cntMilli, LOW);  // Increasing duty cycle on 3..7
    if ((milli%12) == 1) {
        // RC servo pulse from 1 .. 2 millis in a 12 milli period
        digitalWrite(14 + cntMilli, LOW); // RC servo control
-       digitalWrite(19, LOW); // RC servo control
    }
-   if (cntMilli++ < 5)
+   cntMilli++;
+   if (cntMilli < 5)
       return;
    cntMilli = 0;
+   milli++;
+
    digitalWrite(3, HIGH);  // Increasing duty cycle on 3..7
    digitalWrite(4, HIGH);  // Increasing duty cycle on 3..7
    digitalWrite(5, HIGH);  // Increasing duty cycle on 3..7
    digitalWrite(6, HIGH);  // Increasing duty cycle on 3..7
    digitalWrite(7, HIGH);  // Increasing duty cycle on 3..7
 
-   milli++;
-   cntSec++;
    digitalWrite( 8, (milli&4) ? HIGH : LOW); // 125 Hz for stepper /16 ~>  8 steps/sec
    digitalWrite( 9, (milli&2) ? HIGH : LOW); // 250 Hz for stepper /16 ~> 16 steps/sec
    digitalWrite(10, (milli&1) ? HIGH : LOW); // 500 Hz for stepper /16 ~> 32 steps/sec
@@ -68,13 +72,18 @@ void loop(){
       digitalWrite(16, HIGH); // RC servo control
       digitalWrite(17, HIGH); // RC servo control
       digitalWrite(18, HIGH); // RC servo control
-      digitalWrite(19, HIGH); // RC servo control
+
    }
 
-
+   cntSec++;
    if (cntSec < 1000)
       return;
    cntSec = 0;
+   if ((sec%60) == 0) {
+      Serial.println(""); Serial.print("SEC: "); Serial.print(sec); Serial.print(" ");
+   }
+   Serial.print(".");
+   sec++;
    digitalWrite(11, (sec&4) ? HIGH : LOW); // Stepper dir. 1/8 Hz
    digitalWrite(12, (sec&2) ? HIGH : LOW); // Stepper dir. 1/4 Hz
    digitalWrite(13, (sec&1) ? HIGH : LOW); // Stepper dir. 1/2 Hz -- Also the LED
